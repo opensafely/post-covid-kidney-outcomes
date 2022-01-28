a5665f04db32457b4b96661408ff96a986ec3165
viyaasan <viyaasan@hotmail.com>
2022-01-28 15:45:44 +0000

    Study definitions
    Copied/pasted from Gitpod into Github desktop

---
+++ analysis/study_definition_potential_historical_general_population.py
@@ -0,0 +1,46 @@
+from cohortextractor import (
+    StudyDefinition,
+    Measure,
+    patients,
+    codelist,
+    combine_codelists,
+    filter_codes_by_category, #(from OS documentation)
+    codelist_from_csv,
+)
+
+from codelists import *
+
+from common_variables import generate_common_variables
+
+(
+    outcome_variables,
+    demographic_variables,
+    clinical_variables,
+) = generate_common_variables(index_date_variable="patient_index_date")
+
+study = StudyDefinition(
+    default_expectations={
+        "date": {"earliest": "1980-01-01", "latest": "today"},
+        "rate": "uniform",
+        "incidence": 0.7,
+    },
+    population=patients.satisfying(
+        """
+            has_follow_up
+        AND (age >=18)
+        AND (sex = "M" OR sex = "F")
+        AND imd > 0
+        AND NOT stp = ""
+        """,
+        has_follow_up=patients.registered_with_one_practice_between(
+            "patient_index_date - 3 months", "patient_index_date"
+        ),
+    ),
+    index_date="2018-02-01",
+
+#Anyone with a code for dialysis or kidney transplant or with eGFR <15 before 2018-02-01 will be excluded
+
+    ),
+
+    **demographic_variables,
+#Only matching variables need to be extracted = age, sex, STP, IMD
