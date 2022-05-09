5a110f692444025a8be2479d62ee6d42a843f92e
Viyaasan Mahalingasivam <viyaasan@hotmail.com>
2022-05-09 15:48:12 +0100

    Removed non-runnable code from common_variables
    Removed code from common_variables into new file.

--- analysis/common_variables.py
+++ analysis/common_variables.py
@@ -121,18 +121,6 @@ hospital_kidney_replacement_therapy_date=patients.minimum_of(
     "kidney_replacement_therapy_icd_10", "kidney_replacement_therapy_opcs_4"
 ),
 
-#I don't think the following will work
-#Is it possible to use "OR"?
-#I.e. covid_codes AND kidney_replacement_therapy_icd_10 OR kidney_replacement_therapy_opcs_4
-covid_hospitalised_dialysis=patients.admitted_to_hospital(
-    with_these_diagnoses=covid_codes AND hospital_kidney_replacement_therapy_date
-    returning="date_admitted",
-    between = ["covid_diagnosis_date", "covid_diagnosis_date + 28 days"],
-    date_format="YYYY-MM-DD",
-    find_first_match_in_period=True,
-    return_expectations={"incidence": 0.005, "date": {"earliest": "index_date"}},
-),    
-
 covid_acute_kidney_injury=patients.categorised_as(
     {
     "0": "DEFAULT",
@@ -231,42 +219,6 @@ covid_vax_4_date = patients.with_vaccination_record(
     },
 ),
 
-unvaccinated_covid = patients.satisfying(
-    ""
-        sars_cov_2 > "0"
-    AND covid_vax_1_date = "0"
-    ""
-        between = ["2020-11-01", "covid_diagnosis_date - 7 days"]
-        return_expectations={
-            "incidence": 0.4,},
-),
-single_vaccinated_covid = patients.satisfying(
-    ""
-        sars_cov_2 > "0"
-    AND covid_vax_2_date = "0"
-    ""
-        between = ["2020-11-01", "covid_diagnosis_date - 7 days"]
-        return_expectations={
-            "incidence": 0.2,},
-),
-double_vaccinated_covid = patients.satisfying(
-    ""
-        sars_cov_2 > "0"
-    AND covid_vax_3_date = "0"
-    ""
-        between = ["2020-11-01", "covid_diagnosis_date - 7 days"]
-        return_expectations={
-            "incidence": 0.2,},
-),
-triple_vaccinated_covid = patients.satisfying(
-    ""
-        sars_cov_2 > "0"
-    AND covid_vax_3_date < "covid_diagnosis_date - 7 days"
-    ""
-        return_expectations={
-            "incidence": 0.2,},
-),
-
 #Matching variables
 month_of_birth=patients.date_of_birth(
     date_format=None, 
@@ -348,7 +300,7 @@ died_date_gp=patients.with_death_recorded_in_primary_care(
 #Creatinine as of 2020-02-01
 #NB missing floats/integers will be returned as 0 by default
 creatinine_february_2020=patients.most_recent_creatinine(
-    on_or_before="2020-02-01"
+    on_or_before="2020-02-01",
     include_measurement_date=True,
     date_format="YYYY-MM-DD",
     return_expectations={
