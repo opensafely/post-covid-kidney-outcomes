ebc92d9955c4fb201ff5288466f5e4085c1c2cf4
Viyaasan Mahalingasivam <viyaasan@hotmail.com>
2022-10-20 18:13:36 +0100

    Historical matching files

---
+++ analysis/match_historical_stp49.py
@@ -0,0 +1,26 @@
+#https://5b5368c1.opensafely-docs.pages.dev/case-control-studies/
+#https://github.com/opensafely/documentation/pull/522
+
+#https://github.com/opensafely-core/matching#readme:
+import pandas as pd
+from osmatching import match
+
+match(
+    case_csv="input_covid_matching_stp49",
+    match_csv="input_historical_matching_stp49",
+    matches_per_case=5,
+    match_variables={
+        "male": "category",
+        "year_of_birth": 0,
+        "imd": "category",
+    },
+    index_date_variable="covid_date",
+    replace_match_index_date_with_case="3_years_earlier",
+    date_exclusion_variables={
+        "death_date": "before",
+        "date_deregistered": "before",
+        "krt_outcome_date": "before",
+    },
+    output_suffix="_historical_stp49",
+    output_path="output",
+)
